17ab6135f399d9cdfff24ba886ae09b5f5dfe258
Eduardo Rodríguez <erodriguez.cl@gmail.com>
2022-05-17 01:15:56 -0400

    problemas chapter

--- pkg/utils/sqlutils/sqltemplate.go
+++ pkg/utils/sqlutils/sqltemplate.go
@@ -7,8 +7,8 @@ import (
 )
 
 type SqlTemplate[T any] interface {
-	QueryForArray(query string, params []interface{}) ([]T, error)
-	QueryForOne(query string, params []interface{}) (*T, error)
+	QueryForArray(query string, params []interface{}, mapperFunc func(rows *sql.Rows) (T, error)) ([]T, error)
+	QueryForOne(query string, params []interface{}, mapperFunc func(row *sql.Row) (T, error)) (*T, error)
 	Update(sql string, params []interface{}) (interface{}, error)
 }
 
@@ -21,24 +21,59 @@ func NewSqlTemplate[T any](db *sql.DB, txManager transaction.TxManager[*sql.Tx])
 	return &defaultImpl[T]{db, txManager}
 }
 
-func (impl *defaultImpl[T]) QueryForArray(query string, params []interface{}) ([]T, error) {
-
+func (impl *defaultImpl[T]) QueryForArray(query string, params []interface{}, mapperFunc func(rows *sql.Rows) (T, error)) ([]T, error) {
 	tx, err := impl.txManager.GetTx()
 	if err != nil {
 		return nil, err
 	}
 
+	var rows *sql.Rows
+
 	if tx != nil {
-		return nil, nil
+		rows, err = tx.Query(query, params...)
+	} else {
+		rows, err = impl.db.Query(query, params...)
 	}
 
-	// aqui cierra conexion
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var outputArray []T
+	for rows.Next() {
+		aux, err := mapperFunc(rows)
+		if err != nil {
+			return nil, err
+		}
+		outputArray = append(outputArray, aux)
+	}
+	return outputArray, nil
+}
 
-	return nil, nil
+func (impl *defaultImpl[T]) QueryForOne(query string, params []interface{}, mapperFunc func(row *sql.Row) (T, error)) (*T, error) {
+	tx, err := impl.txManager.GetTx()
+	if err != nil {
+		return nil, err
+	}
+
+	var row *sql.Row
+
+	if tx != nil {
+		row = tx.QueryRow(query, params...)
+	} else {
+		row = impl.db.QueryRow(query, params...)
+	}
+
+	aux, err := mapperFunc(row)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &aux, nil
 }
-func (impl *defaultImpl[T]) QueryForOne(query string, params []interface{}) (*T, error) {
-	return nil, nil
-}
+
 func (impl *defaultImpl[T]) Update(sql string, params []interface{}) (interface{}, error) {
 	return nil, nil
 }
